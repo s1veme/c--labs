8fbfc0ae1ce466ffa11ecad6c5a269ce8de4915f
Андрей Падерин <kpdpav@gmail.com>
2024-03-16 12:34:04 +0700

    fix: fix font error

--- unit/UnitTest1.cs
+++ unit/UnitTest1.cs
@@ -7,20 +7,20 @@ public class CalculatorServiceTests
     public void TestDivisionByZero()
     {
         List<double> results = CalculatorService.Calculate(1, 0, 10);
-        Assert.IsTrue(results.Count < 10, "Ошибка: Деление на ноль");
+        Assert.IsTrue(results.Count < 10, "Error: Division by zero");
     }
 
     [TestMethod]
     public void TestRangeOverflow()
     {
         List<double> results = CalculatorService.Calculate(1, 1, 10);
-        Assert.IsTrue(results.Count < 10, "Ошибка: Выход за пределы диапазона.");
+        Assert.IsTrue(results.Count < 10, "Error: Out of range");
     }
 
     [TestMethod]
     public void TestPositive()
     {
         List<double> results = CalculatorService.Calculate(0.5, 1, 10);
-        Assert.IsTrue(results.Last() == -2, "Ошибка: Успешный тест не прошёл");
+        Assert.IsTrue(results.Last() == -2, "Error: The successful test failed");
     }
 }
